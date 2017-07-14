0b3ee4ef9217a6db5e359423505e175b6bc8cce5
Lydia Morneault <lydia.morneault@gmail.com>
2017-07-14 10:30:41 -0400

    Converted Actor and Scene00 classes

---
+++ Actor.java
@@ -0,0 +1,22 @@
+
+public class Actor {
+	public int hitPoints;
+	public int maxHitPoints;
+	public int strength;
+	public int maxStrength;
+	public int potions;
+	public int gold;
+	public String name;
+	
+	public Actor(){
+		hitPoints = 0;
+		maxHitPoints = 0;
+		strength = 0;
+		maxStrength = 0;
+		potions = 0;
+		gold = 0;
+		name = "";
+	}
+	
+	
+}
---
+++ Scene00.java
@@ -0,0 +1,16 @@
+public class Scene00 {
+	public static Actor hero = new Actor();
+
+	public static void main(String[] args) {
+		System.out.println("\n\n\n\nYou enter the mine and walk a few feet.");
+		System.out.println("You find a piece of gold and pick it up.");
+		
+		//TODO Make actor class
+		hero.gold++;
+		
+		System.out.println("It makes a pleasant clink in your bag of treasure.");
+		System.out.println("\nYou continue walking into the mine.");
+		System.out.println("Everything is fine, so far.");
+	}
+
+}
