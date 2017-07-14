1143f7306045375635abef5b540f1bc43ffc2274
Asher <asherb@my.ccsu.edu>
2017-07-14 10:15:55 -0400

    Add files via upload
    All C programs

---
+++ scene06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "actor.h"
+#include "utilities.h"
+
+extern char buffer[132]; 
+extern actor hero;
+extern actor monster;
+ 
+void scene06( int monsterHit, int monsterStr )
+{
+
+  printf("  You continue deeper into the mine.\n\n");
+  printf("  Nothing happens\n\n ");
+  
+}
